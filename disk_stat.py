7cc93d4a265be5920917a288f3c47924d0d64944
deepamartin <deepa.martin@maplelabs.com>
2018-05-21 00:12:35 -0700

    set avgQueueSize to 0.0 when the values are nto applicaton for the field

--- disk_stat.py
+++ disk_stat.py
@@ -98,6 +98,8 @@ class DiskStats(object):
             avgqusizedata = dict_iostats.get(name)
             if avgqusizedata:
                avgqusizedata = float(avgqusizedata)
+            else:
+               avgqusizedata = 0.0
             disk = {READBYTE: float(disk_ioinfo.read_bytes) / (FACTOR * FACTOR), WRITEBYTE: float(
                 disk_ioinfo.write_bytes) / (FACTOR * FACTOR), READCOUNT: disk_ioinfo.read_count,
                     WRITECOUNT: disk_ioinfo.write_count, READTIME: disk_ioinfo.read_time,
@@ -190,6 +192,7 @@ class DiskStats(object):
         if agg_usage:
             disk[AGG + USAGE] = agg_usage
 
+        disk[AVGQUEUESIZE] = 0.0
         dict_disks[AGGREGATE] = disk
 
     def add_common_params(self, dict_disks):
