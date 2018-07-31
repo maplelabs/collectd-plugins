ed9be9042704203bc161723acdd9380aadf38560
KavishreeShanmugam <kavishree.ece@gmail.com>
2018-07-31 14:02:58 +0530

    Added ESA plugin to download log file from ESA cluster

--- esa.py
+++ esa.py
@@ -34,35 +34,61 @@ class ESALogs:
                 self.interval = children.values[0]
             if children.key == "log_name":
                 self.log_name = children.values[0]
+                self.dummy_path = '/tmp/'+self.log_name
 
     def checkLogDest(self):
         try:
             if not os.path.exists(self.download_path):
-                os.makedirs('esa_logs')
+                os.makedirs(self.download_path)
         except Exception as err:
             collectd.error("Error in creating esa log folder %s" %(str(err)))
             return False
         return True
 
-    def collect_log(self):
-        final_details = {}
+    @staticmethod
+    def fileCompare(esa_log_file, dummy_file):
         try:
-            if (self.checkLogDest()):
+            f=open(esa_log_file, "a")
+            line = subprocess.check_output(['tail', '-1', esa_log_file])
+            line_num = 0
+            with open(dummy_file) as f1:
+                dwl_log_line = f1.readlines()
+            for i in range(len(dwl_log_line)):
+                if dwl_log_line[i] == line:
+                    line_num = i+1
+                    break
+            with open(dummy_file) as f2:
+                for i in xrange(line_num):
+                    f2.next()
+                for log_line in f2:
+                    f.write(log_line)
+            f.close()
+            return True
+        except Exception as err:
+            collectd.error("Error in copying the log file due to %s" %(str(err)))
+            return False
+
+    def collect_log(self):
+        data_dict = {}
+        try:
+            if self.checkLogDest() and self.log_name:
                 esa_url = 'ftp://{0}:{1}@{2}/{3}/{4}.current'.format(self.user, self.password, self.host, self.log_name, log_name_mapping[self.log_name])
-                download_path = self.download_path + self.log_name + '.txt'
-                urllib.urlretrieve(esa_url, download_path)
+                urllib.urlretrieve(esa_url, self.dummy_path)
                 #urllib.urlretrieve('ftp://admin:ironport@10.11.100.82/'+ self.log_name +'/'+ log_name_mapping[self.log_name] +'.current', '/var/log/esa_logs/'+ self.log_name+'.txt')
                 urllib.urlcleanup()
+                if not self.fileCompare(esa_log_file = self.download_path+self.log_name, dummy_file=self.dummy_path):
+                    return
             else:
-                final_details["status"] = "failed"
                 collectd.info("Couldn't store the data")
-                return final_details
+                return
         except Exception as e:
             collectd.error("Error in downloading log files due to %s" % (str(e)))
-            return
-        final_details["status"] = "success"
+            data_dict["status"] = "Failed"
+            data_dict["message"] = e
+            return data_dict
+        data_dict["status"] = "success"
         collectd.info("Downloaded Log files from ESA cluster %s " %(str(data_dict)))
-        return final_details
+        return data_dict
 
     @staticmethod
     def add_common_params(result_dict):
@@ -72,12 +98,12 @@ class ESALogs:
         result_dict[TIMESTAMP] = timestamp
         result_dict[PLUGIN] = "esa"
         result_dict[ACTUALPLUGINTYPE] = "esa"
-        result_dict[PLUGINTYPE] = self.log_name
+        result_dict[PLUGINTYPE] = "esa"
         collectd.info("Plugin ESALogs: Added common parameters successfully")
 
     @staticmethod
     def dispatch_data(result_dict):
-        collectd.debug("Plugin ESALogs: Values dispatched = " + json.dumps(result_dict))
+        collectd.info("Plugin ESALogs: Values dispatched %s" %(str(result_dict)))
         dispatch(result_dict)
 
     def read(self):
