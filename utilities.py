29b815749c44c44ee2afeedf2c4aa34299f360a6
siddaramm <siddaram.sonar@maplelabs.com>
2018-11-29 01:11:42 -0500

    Hadoop Plugins

--- utilities.py
+++ utilities.py
@@ -4,11 +4,8 @@ from datetime import datetime
 import json
 import os
 import time
-import logging
 import math
 
-
-logger = logging.getLogger("__name__")
 
 
 def get_unix_timestamp(date_text):
@@ -26,11 +23,4 @@ def convert_camelcase(str_to_convert, separator):
     c = ''.join(x for x in str_to_convert.title() if not x == separator)
     c = c[0].lower() + c[1::]
     return c
-
-
-def write_json_to_file(data, outfile):
-    if os.path.exists(outfile):
-        with open(outfile, 'a') as ofile:
-            json.dump(data, ofile)
-            ofile.write("\n")
 
