786ac2a2906b8de0813a5c60c83dc1c50ada127a
root <root@ip-172-31-36-133.us-west-2.compute.internal>
2019-01-06 09:47:50 +0000

    Latest spark code

--- sf-plugins-hadoop/Collectors/configuration.py
+++ sf-plugins-hadoop/Collectors/configuration.py
@@ -1,54 +1,18 @@
-oozie = {
-    "scheme": "http",
-    "host": "10.81.1.98",
-    "port": 11000,
-    "jobs_size" : 250
-}
+oozie = {'host': '10.81.1.98', 'scheme': 'http', 'port': '11000', 'jobs_size': 250}
 
-elastic = {
-    "scheme": "http",
-    "host": "10.81.1.212",
-    "port": "9200"
-}
+elastic = {'host': '10.81.1.212', 'scheme': 'http', 'port': '9200'}
 
-timeline_server = {
-    "scheme": "http",
-    "host" : "pndademocloud-hadoop-mgr-1",
-    "port" :  8188
-}
+timeline_server = {'host': '10.81.1.98', 'scheme': 'http', 'port': '8188'}
 
-job_history_server = {
-    "scheme": "http",
-    "host": "10.81.1.183",
-    "port": 19888
-}
+job_history_server = {'host': '10.81.1.183', 'scheme': 'http', 'port': '19888'}
 
-resource_manager = {
-    "scheme": "http",
-    "hosts": ["10.81.1.242", "10.81.1.160"],
-    "port": "8088"
-}
+resource_manager = {'scheme': 'http', 'hosts': ['10.81.1.98', '10.81.1.104'], 'port': '8088'}
 
-name_node = {
-    "scheme": "http",
-    "hosts": ["10.81.1.160", "10.81.1.242"],
-    "port" : 50070
-}
+name_node = {'scheme': 'http', 'hosts': ['10.81.1.98', '10.81.1.104'], 'port': '50070'}
 
-indices = {
-    "workflow": "snappyflow_write",
-    "workflowmonitor": "workflowmonitornew",
-    "namenode": "hadoop-new",
-    "yarn": "hadoop-new",
-    "spark": "hadoop-new"
-}
+indices = {'namenode': 'snappyflow_write', 'spark': 'snappyflow_write', 'workflowmonitor': 'workflowmonitornew', 'yarn': 'snappyflow_write', 'workflow': 'snappyflow_write'}
 
-tag_app_name = {
-   "oozie" : "hadoopapp1",
-   "yarn" : "hadoop-new",
-   "namenode": "hadoop-new",
-    "spark" : "hadoop-new"
-}
+tag_app_name = {'namenode': 'hadoopapp1', 'spark': 'hadoopapp1', 'yarn': 'hadoopapp1', 'oozie': 'hadoopapp1'}
 
 plugin_name = {
    "oozie" : "oozie",
@@ -69,8 +33,8 @@ logging_config = {
     "namenode": "./var/log/namenode.log"
 }
 
-previous_json_yarn = './var/previous_json_yarn'
-previous_json_nn = './var/previous_json_nn'
+previous_json_yarn = "/var/previous_json_yarn"
+previous_json_nn = "/var/previous_json_nn"
 
 
 sleep_time_in_secs = {
@@ -93,21 +57,17 @@ kerberos = {
 }
 
 hdfs_jobhistory_directory = "/mr-history/done"
-jobhistory_copy_dir = "./var/jhist"
+jobhistory_copy_dir = "/var/jhist"
 hueristics_out_dir = "./var/hueristics"
-use_rest_api = False
+use_rest_api = 0
 
-hdfs = {
-    "url" : "http://10.81.1.98:50070;http:// 10.81.1.104:50070",
-    "user" : "root",
-    "timezone" : "US/Eastern"
-}
+hdfs = {'url': 'http://10.81.1.98:50070;http://10.81.1.104:50070', 'timezone': 'US/Eastern', 'user': 'root'}
 
 modify_user_agent_header = True
 
 spark2_history_server = {
     "scheme": "http",
-    "host": "10.81.1.215",
+    "host": "10.81.1.183",
     "port": 18081
 }
 
@@ -131,7 +91,10 @@ tasks_by_time = {
 }
 
 app_status = {
-    'use_redis' : True
+    'use_redis' : True,
+    'oozie-key': "oozieStatus",
+    'spark-key': "sparkStatus"
+
 }
 
 redis_server = {
