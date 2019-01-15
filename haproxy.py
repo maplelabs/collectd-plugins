b31b6e7d65fbfbabe69ffa94068c521c2cab3574
krishna-maplelabs <krishna.kulkarni@maplelabs.com>
2019-01-15 19:13:24 +0530

    Added function to get diff values in haproxy plugin
    Added and tested:
    Function to get difference values for few fields
    New document type to collect server info called 'haproxyStats'

--- haproxy.py
+++ haproxy.py
@@ -1,13 +1,10 @@
-""" A collectd-python plugin for retrieving
-    metrics from haproxy server.
-"""
 import json
 import time
 import collectd
 import subprocess
 import re
-import traceback
 import signal
+import traceback
 from collections import defaultdict
 from copy import deepcopy
 
@@ -15,7 +12,7 @@ from copy import deepcopy
 import utils
 from constants import *
 
-HAPROXY_DOCS = ["frontendStats", "backendStats"]
+HAPROXY_DOCS = ["frontendStats", "backendStats", "haproxyStats"]
 
 class haproxyStats(object):
     """Plugin object will be created only once and collects utils
@@ -25,6 +22,10 @@ class haproxyStats(object):
         """Initializes interval."""
         self.interval = DEFAULT_INTERVAL
         self.documentsTypes = []
+        self.pollCounter = 0
+        self.prev_frontend_data = {}
+        self.prev_backend_data = {}
+        self.prev_haproxy_data = {}
 
     def config(self, cfg):
         """Initializes variables from conf files."""
@@ -64,14 +65,14 @@ class haproxyStats(object):
                         i += 1
 
             if metrics_tmp:
-                haproxy_data['frontendStats']['httpReqRate'] = metrics_tmp['http_req_rate']
-                haproxy_data['frontendStats']['httpRate'] = metrics_tmp['http_rate']
-                haproxy_data['frontendStats']['httpEreq'] = metrics_tmp['http_ereq']
-                haproxy_data['frontendStats']['httpDreq'] = metrics_tmp['http_dreq']
-                haproxy_data['frontendStats']['httpHrsp_4xx'] = metrics_tmp['http_hrsp_4xx']
-                haproxy_data['frontendStats']['httpHrsp_5xx'] = metrics_tmp['http_hrsp_5xx']
-                haproxy_data['frontendStats']['httpBin'] = metrics_tmp['http_bin']
-                haproxy_data['frontendStats']['httpBout'] = metrics_tmp['http_bout']
+                haproxy_data['frontendStats']['httpReqRate'] = float(metrics_tmp['http_req_rate'])
+                haproxy_data['frontendStats']['httpRate'] = float(metrics_tmp['http_rate'])
+                haproxy_data['frontendStats']['httpEreq'] = int(metrics_tmp['http_ereq'])
+                haproxy_data['frontendStats']['httpDreq'] = int(metrics_tmp['http_dreq'])
+                haproxy_data['frontendStats']['httpHrsp_4xx'] = int(metrics_tmp['http_hrsp_4xx'])
+                haproxy_data['frontendStats']['httpHrsp_5xx'] = int(metrics_tmp['http_hrsp_5xx'])
+                haproxy_data['frontendStats']['httpBin'] = float(metrics_tmp['http_bin']) / 1024
+                haproxy_data['frontendStats']['httpBout'] = float(metrics_tmp['http_bout']) / 1024
                 haproxy_data['frontendStats']['httpSutil'] = (float(metrics_tmp['http_scur']) / float(metrics_tmp['http_slim'])) * 100
                 haproxy_data['frontendStats']['_documentType'] = 'frontendStats'
 
@@ -97,15 +98,17 @@ class haproxyStats(object):
                         i += 1
 
             if metrics_tmp:
-                haproxy_data['backendStats']['httpRtime'] = metrics_tmp['http_rtime']
-                haproxy_data['backendStats']['httpEcon'] = metrics_tmp['http_econ']
-                haproxy_data['backendStats']['httpDresp'] = metrics_tmp['http_dresp']
-                haproxy_data['backendStats']['httpEresp'] = metrics_tmp['http_eresp']
-                haproxy_data['backendStats']['httpQcur'] = metrics_tmp['http_qcur']
-                haproxy_data['backendStats']['httpQtime'] = metrics_tmp['http_qtime']
-                haproxy_data['backendStats']['httpWredis'] = metrics_tmp['http_wredis']
-                haproxy_data['backendStats']['httpWretr'] = metrics_tmp['http_wretr']
+                haproxy_data['backendStats']['httpRtime'] = int(metrics_tmp['http_rtime'])
+                haproxy_data['backendStats']['httpEcon'] = int(metrics_tmp['http_econ'])
+                haproxy_data['backendStats']['httpDresp'] = int(metrics_tmp['http_dresp'])
+                haproxy_data['backendStats']['httpEresp'] = int(metrics_tmp['http_eresp'])
+                haproxy_data['backendStats']['httpQcur'] = int(metrics_tmp['http_qcur'])
+                haproxy_data['backendStats']['httpQtime'] = int(metrics_tmp['http_qtime'])
+                haproxy_data['backendStats']['httpWredis'] = int(metrics_tmp['http_wredis'])
+                haproxy_data['backendStats']['httpWretr'] = int(metrics_tmp['http_wretr'])
                 haproxy_data['backendStats']['_documentType'] = 'backendStats'
+
+
 
             else:
                 collectd.error("Plugin haproxy: Error collecting backend stats for http proxy")
@@ -116,26 +119,184 @@ class haproxyStats(object):
         except Exception as err:
             collectd.error("Plugin haproxy: Exception in get_backend_data due to %s" % err)
 
-    def collect_haproxy_data(self):
+    def get_haproxy_data(self, lines, haproxy_data):
+        dict_stats = {}
+
         try:
-            dict_stats = defaultdict(list)
-            key_mapping = []
+            for line in lines:
+                line = line.strip("\n")
+                if line != b'':
+                    buf = line.split(":")
+                    dict_stats[buf[0]] = buf[1].strip(" ")
+
+            haproxy_data['haproxyStats']['version'] = dict_stats['Version']
+            haproxy_data['haproxyStats']['upTime'] = int(dict_stats['Uptime_sec'])
+            haproxy_data['haproxyStats']['currConns'] = int(dict_stats['CurrConns'])
+            haproxy_data['haproxyStats']['connRate'] = int(dict_stats['ConnRate'])
+            haproxy_data['haproxyStats']['nbproc'] = int(dict_stats['Nbproc'])
+            haproxy_data['haproxyStats']['pipesUsed'] = int(dict_stats['PipesUsed'])
+            haproxy_data['haproxyStats']['sslCacheMisses'] = int(dict_stats['SslCacheMisses'])
+            haproxy_data['haproxyStats']['sslCacheLookups'] = int(dict_stats['SslCacheLookups'])
+            haproxy_data['haproxyStats']['sessRate'] = int(dict_stats['SessRate'])
+            haproxy_data['haproxyStats']['_documentType'] = "haproxyStats"
+
+            self.add_common_params(haproxy_data['haproxyStats'], 'haproxyStats')
+
+        except Exception as err:
+            collectd.error("Plugin haproxy: Exception in get_haproxy_data due to %s" % err)
+
+    def collect_haproxy_data(self, doc):
+        try:
             haproxy_data = defaultdict(dict)
+            if doc == "frontendStats" or doc == "backendStats":
+                dict_stats = defaultdict(list)
+                key_mapping = []
 
-            cmnd = "echo 'show stat' | nc -U /var/lib/haproxy/stats"
-            process = subprocess.Popen(cmnd, shell=True, stdout=subprocess.PIPE)
-            lines = process.stdout.readlines()
+                cmnd = "echo 'show stat' | nc -U /var/lib/haproxy/stats"
+                process = subprocess.Popen(cmnd, shell=True, stdout=subprocess.PIPE)
+                lines = process.stdout.readlines()
 
-            self.get_keys(key_mapping, lines)
-            self.format_stats(dict_stats, lines)
+                self.get_keys(key_mapping, lines)
+                self.format_stats(dict_stats, lines)
 
-            self.get_frontend_data(key_mapping, dict_stats, haproxy_data)
-            self.get_backend_data(key_mapping, dict_stats, haproxy_data)
+                self.get_frontend_data(key_mapping, dict_stats, haproxy_data)
+                self.get_backend_data(key_mapping, dict_stats, haproxy_data)
+
+            if doc == "haproxyStats":
+                cmnd = "echo 'show info' | nc -U /var/lib/haproxy/stats"
+                process = subprocess.Popen(cmnd, shell=True, stdout=subprocess.PIPE)
+                lines = process.stdout.readlines()
+
+                self.get_haproxy_data(lines, haproxy_data)
 
             return haproxy_data
 
         except Exception as err:
             collectd.error("Plugin haproxy: Exception in collect_haproxy_data due to %s" % err)
+
+    def add_default_diff_value(self, doc_stats, doc):
+        if doc == 'frontendStats':
+            keylist = ['httpEreq', 'httpDreq', 'httpHrsp_4xx', 'httpHrsp_5xx', 'httpBin', 'httpBout']
+
+        elif doc == 'backendStats':
+            keylist = ['httpEcon', 'httpDresp', 'httpEresp', 'httpWredis', 'httpWretr']
+
+        elif doc == 'haproxyStats':
+            keylist = ['sslCacheMisses', 'sslCacheLookups']
+
+        for key in keylist:
+            doc_stats[key] = 0
+
+    def get_diff(self, key, curr_data, prev_data):
+        diff = NAN
+        if not prev_data:
+            return diff
+
+        if key not in prev_data:
+            collectd.error("%s key not in previous data. Shouldn't happen." % key)
+            return diff
+
+        if TIMESTAMP not in curr_data or TIMESTAMP not in prev_data:
+            collectd.error("%s key not in previous data. Shouldn't happen." % key)
+            return diff
+
+        curr_time = curr_data[TIMESTAMP]
+        prev_time = prev_data[TIMESTAMP]
+
+        if curr_time <= prev_time:
+            collectd.error("Current data time: %s is less than previous data time: %s. "
+                           "Shouldn't happen." % (curr_time, prev_time))
+            return diff
+
+        diff = curr_data[key] - prev_data[key]
+        # rate can get negative if the topic(s) are deleted and created again with the same name
+        # intializing rate to 0 if rates are negative value.
+        # if diff < 0:
+        #    rate = 0.0
+        return diff
+
+    def add_diff(self, doc_stats, doc):
+        if doc == 'frontendStats':
+            diff = self.get_diff('httpEreq', doc_stats, self.prev_frontend_data)
+            if diff != NAN:
+                doc_stats['httpEreq'] = diff
+
+            diff = self.get_diff('httpDreq', doc_stats, self.prev_frontend_data)
+            if diff != NAN:
+                doc_stats['httpDreq'] = diff
+
+            diff = self.get_diff('httpHrsp_4xx', doc_stats, self.prev_frontend_data)
+            if diff != NAN:
+                doc_stats['httpHrsp_4xx'] = diff
+
+            diff = self.get_diff('httpHrsp_5xx', doc_stats, self.prev_frontend_data)
+            if diff != NAN:
+                doc_stats['httpHrsp_5xx'] = diff
+
+            diff = self.get_diff('httpBin', doc_stats, self.prev_frontend_data)
+            if diff != NAN:
+                doc_stats['httpBin'] = round(diff, 2)
+
+            diff = self.get_diff('httpBout', doc_stats, self.prev_frontend_data)
+            if diff != NAN:
+                doc_stats['httpBout'] = round(diff, 2)
+
+        if doc == 'backendStats':
+            diff = self.get_diff('httpEcon', doc_stats, self.prev_backend_data)
+            if diff != NAN:
+                doc_stats['httpEcon'] = diff
+
+            diff = self.get_diff('httpDresp', doc_stats, self.prev_backend_data)
+            if diff != NAN:
+                doc_stats['httpDresp'] = diff
+
+            diff = self.get_diff('httpEresp', doc_stats, self.prev_backend_data)
+            if diff != NAN:
+                doc_stats['httpEresp'] = diff
+
+            diff = self.get_diff('httpWredis', doc_stats, self.prev_backend_data)
+            if diff != NAN:
+                doc_stats['httpWredis'] = diff
+
+            diff = self.get_diff('httpWretr', doc_stats, self.prev_backend_data)
+            if diff != NAN:
+                doc_stats['httpWretr'] = diff
+
+        if doc == 'haproxyStats':
+            diff = self.get_diff('sslCacheMisses', doc_stats, self.prev_haproxy_data)
+            if diff != NAN:
+                doc_stats['sslCacheMisses'] = diff
+
+            diff = self.get_diff('sslCacheLookups', doc_stats, self.prev_haproxy_data)
+            if diff != NAN:
+                doc_stats['sslCacheLookups'] = diff
+
+    def add_dispatch_haproxy(self, doc_stats, doc):
+        if doc == 'frontendStats':
+            if self.pollCounter == 1:
+                self.prev_frontend_data = deepcopy(doc_stats)
+                self.add_default_diff_value(doc_stats, doc)
+            else:
+                self.add_diff(doc_stats, doc)
+                self.prev_frontend_data = deepcopy(doc_stats)
+
+        elif doc == 'backendStats':
+            if self.pollCounter == 1:
+                self.prev_backend_data = deepcopy(doc_stats)
+                self.add_default_diff_value(doc_stats, doc)
+            else:
+                self.add_diff(doc_stats, doc)
+                self.prev_backend_data = deepcopy(doc_stats)
+
+        elif doc == 'haproxyStats':
+            if self.pollCounter == 1:
+                self.prev_haproxy_data = deepcopy(doc_stats)
+                self.add_default_diff_value(doc_stats, doc)
+            else:
+                self.add_diff(doc_stats, doc)
+                self.prev_haproxy_data = deepcopy(doc_stats)
+
+        self.dispatch_data(deepcopy(doc_stats), doc)
 
     def add_common_params(self, dict_stats, doc):
         """Adds TIMESTAMP, PLUGIN, PLUGITYPE to dictionary."""
@@ -150,28 +311,29 @@ class haproxyStats(object):
     def read(self):
         """Collects all data."""
         try:
-            haproxy_stats = self.collect_haproxy_data()
+            self.pollCounter += 1
+            for doc in HAPROXY_DOCS:
+                haproxy_stats = self.collect_haproxy_data(doc)
 
-            if not haproxy_stats:
-                collectd.error("Plugin haproxy: Unable to fetch data for haproxy.")
-                return
-            else:
-                for document in haproxy_stats.keys():
-                    if haproxy_stats[document]['_documentType'] not in self.documentsTypes:
-                        del haproxy_stats[document]
+                if not haproxy_stats:
+                    collectd.error("Plugin haproxy: Unable to fetch data for document type: %s." % doc)
+                    return
+                else:
+                    #self.documentsTypes = ['frontendStats', 'backendStats', 'haproxyStats']
+                    if haproxy_stats[doc]['_documentType'] not in self.documentsTypes:
+                            del haproxy_stats[doc]
 
-            self.dispatch_data(deepcopy(haproxy_stats))
+                    self.add_dispatch_haproxy(haproxy_stats[doc], doc)
 
         except Exception as err:
             collectd.error("Plugin haproxy: Couldn't read and gather the metrics due to the exception %s in %s" % (err, traceback.format_exc()))
 
-    def dispatch_data(self, result):
+    def dispatch_data(self, result, doc):
         """Dispatch data to collectd."""
-        for details_type, details in result.items():
-            collectd.info("Plugin haproxy: Succesfully sent %s doctype to collectd." % details_type)
-            collectd.info("Plugin haproxy: Values dispatched =%s" % json.dumps(details))
+        collectd.info("Plugin haproxy: Succesfully sent %s doctype to collectd." % doc)
+        collectd.info("Plugin haproxy: Values dispatched =%s" % json.dumps(result))
 
-            utils.dispatch(details)
+        utils.dispatch(result)
 
     def read_temp(self):
         """Collectd first calls register_read. At that time default interval is taken,
@@ -190,4 +352,3 @@ OBJ = haproxyStats()
 collectd.register_init(init)
 collectd.register_config(OBJ.config)
 collectd.register_read(OBJ.read_temp)
-                                            
