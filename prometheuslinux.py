dbc5282bcf3a3895200a70fcc1fa90a4cfe90415
ramya-govind <ramya_g@Maplelabs.com>
2019-07-17 17:57:16 +0530

    Added prometheus plugins such as linux,jmx, mysql and elasticsearch Added: 	Added child class for different prometheus services Test-details: 	Each prometheus plugin will use base prometheus to poll metrics. 	Collect metrics and send data to ES.

---
+++ prometheuslinux.py
@@ -0,0 +1,43 @@
+"""
+*******************
+*Copyright 2017, MapleLabs, All Rights Reserved.
+*
+********************
+"""
+""" A collectd-python plugin for retrieving
+    metrics from prometheus linux status module. """
+
+import signal
+import collectd
+import prometheus_poller
+from constants import *
+
+
+class PrometheusLinux(prometheus_poller.PrometheusStat):
+
+    def __init__(self):
+        self.interval = DEFAULT_INTERVAL
+        self.port = None
+        self.conf = {}
+
+    def config(self, cfg):
+        for children in cfg.children:
+            if children.key == INTERVAL:
+                self.interval = children.values[0]
+            if children.key == PORT:
+                self.port = children.values[0]
+        self.conf.update({'interval': self.interval, 'port': self.port, 'name': 'prometheuslinux'})
+        super(PrometheusLinux, self).__init__(self.conf)
+
+    def read_temp(self):
+        collectd.unregister_read(self.read_temp)
+        collectd.register_read(self.read, interval=int(self.interval))
+
+
+def init():
+    signal.signal(signal.SIGCHLD, signal.SIG_DFL)
+
+OBJ = PrometheusLinux()
+collectd.register_init(init)
+collectd.register_config(OBJ.config)
+collectd.register_read(OBJ.read_temp)
